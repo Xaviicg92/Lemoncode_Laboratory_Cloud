07da685912825976d25dc50f06397d73fc6f7b23
Javier Cambón <cg_javi@hotmail.com>
2023-01-31 13:26:14 +0100

    Changes Laboratory 7

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { AppRouter } from "./core/router/app.router";
-import { KittiesScene } from "./scenes/kitties.scene";
 
 export const App = () => {
   return <AppRouter />;
